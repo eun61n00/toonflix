c0e17884091b48984b982ba3d8a8035ab84506b1
eun61n00 <eun61n@gmail.com>
2023-09-29 07:53:28 +0900

    Feat: show title of webtoon with ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +26,20 @@ class HomeScreen extends StatelessWidget {
       body: FutureBuilder(
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Text("${snapshot.data}");
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                print(index);
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => SizedBox(width: 20),
+            );
           } else {
-            return Text("Loading...");
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
         },
         future: webtoons,
