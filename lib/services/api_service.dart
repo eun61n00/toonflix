333150abe67d31b6b2b68a45cf4ad05c417b20cc
eun61n00 <eun61n@gmail.com>
2023-09-29 10:23:14 +0900

    Feat: Fetch details and latest episodes of webtoon

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -22,5 +24,31 @@ class ApiService {
       return webtoonInstances;
     }
     throw Exception("Failed to load webtoons");
+  }
+
+  static Future<WebtoonDetailModel> getWebtoonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final detail = WebtoonDetailModel.fromJson(jsonDecode(response.body));
+      return detail;
+    }
+    throw Exception("Feail to load webtoon datail information");
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> webtoonEpisodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        var episodeInstance = WebtoonEpisodeModel.fromJson(episode);
+        webtoonEpisodesInstances.add(episodeInstance);
+      }
+      return webtoonEpisodesInstances;
+    }
+    throw Exception("Feail to load webtoon datail information");
   }
 }
